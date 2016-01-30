054cec18afa68587ff6eb77c3934681a627a56c3
Salvo Cirinà <salvocia90@gmail.com>
2016-01-30 14:09:07 +0100

    testShield

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -5,17 +5,23 @@ public class InputController : MonoBehaviour {
 
 	private Rigidbody rb;
 
+	public static InputController instance;
+
 	public float movSpeed = 25.0f;
-	public float runSpeed = 25.0f;
+	private float runSpeed;
 	public float rotSpeed = 15.0f;
-	public float rotShieldSpeed = 35.0f;
+	public float rotGatlinSpeed = 15.0f;
+	public float rotShieldSpeed = 1.0f;
 	public float rotRocketSpeed = 35.0f;
 
 	public float gatlinShootForce = 15.0f;
 
-	private float lastShoot = 0;
+	private float lastGatlinShoot = 0;
+	private float lastRocketShoot = 0;
 
 	public float gatlingShootingRate = 0.5f;
+	public float rocketShootingRate = 0.5f;
+	private float improvedrocketShootingRate;
 
 	public float minClampGatlin = -45.0f;
 	public float maxClampGatlin = 45.0f;
@@ -38,15 +44,22 @@ public class InputController : MonoBehaviour {
 	public Transform gatlinLeftSpawnPoint;
 	public Transform gatlinRightSpawnPoint;
 	public Transform rocketSpawnPoint;
+
+	public bool canDeflect;
 //	public string veritcalAimGatlinAxis;
 
 	void Awake() {
 
 		rb = GetComponent<Rigidbody>();
-
+		instance = this;
 	}
 
 	void Start () {
+
+		runSpeed = movSpeed * 2.0f;
+
+		improvedrocketShootingRate = rocketShootingRate/2;
+
 		horizontalAxis += gameObject.name;
 		veritcalAxis += gameObject.name;
 
@@ -66,20 +79,28 @@ public class InputController : MonoBehaviour {
 	// Update is called onceper frame
 	void Update () {
 
-		float moveH = Input.GetAxis(horizontalAxis) * movSpeed;
-		float moveV = Input.GetAxis(veritcalAxis) * movSpeed;
-		Move(moveH, moveV);
+
+
+		if(Input.GetAxis("Run") > 0.1f){
+			float moveH = Input.GetAxis(horizontalAxis) * runSpeed;
+			float moveV = Input.GetAxis(veritcalAxis) * runSpeed;
+			Move(moveH, moveV);
+		} else {
+			float moveH = Input.GetAxis(horizontalAxis) * movSpeed;
+			float moveV = Input.GetAxis(veritcalAxis) * movSpeed;
+			Move(moveH, moveV);
+		}
 
 		float aimH = Input.GetAxis(horizontalAimAxis) * rotSpeed;
 		float aimV = Input.GetAxis(veritcalAimAxis) * rotSpeed;
 		Aim(aimH, aimV);
 
-		float aimGatlinH = Input.GetAxis(horizontalAimGatlinAxis) * rotSpeed;
+		float aimGatlinH = Input.GetAxis(horizontalAimGatlinAxis) * rotGatlinSpeed;
 //		float aimGatlinV = Input.GetAxis(veritcalAimGatlinAxis) * rotSpeed;
 		RotateGatlinGun(aimGatlinH);
 		if(Input.GetAxis("GatlinFire") > 0.1f) {
-			if(((Time.time - lastShoot) > gatlingShootingRate)) {
-				lastShoot = Time.time;
+			if(((Time.time - lastGatlinShoot) > gatlingShootingRate)) {
+				lastGatlinShoot = Time.time;
 				GatlinShoot();
 			}
 		}
@@ -93,11 +114,18 @@ public class InputController : MonoBehaviour {
 		RocketsAim(rocketH, rocketV);
 
 		if(Input.GetAxis("RocketFire") > 0.1f) {
-			if(((Time.time - lastShoot) > gatlingShootingRate)) {
-				lastShoot = Time.time;
+			if(((Time.time - lastRocketShoot) > rocketShootingRate)) {
+				lastRocketShoot = Time.time;
+				RocketShoot();
+			}
+		} else if (Input.GetAxis("ImprovedRocketFire") > 0.1f) {
+			if(((Time.time - lastRocketShoot) > improvedrocketShootingRate)) {
+				lastRocketShoot = Time.time;
 				RocketShoot();
 			}
 		}
+
+		EnableBouncingShield();
 
 	}
 
@@ -107,7 +135,7 @@ public class InputController : MonoBehaviour {
 		if (targetDir.sqrMagnitude > 0.21f) {
 			// rotation of the ship
 			var rotation= Quaternion.LookRotation(targetDir, Vector3.up);
-			transform.GetChild(0).rotation = Quaternion.Slerp(transform.GetChild(0).rotation, rotation, Time.deltaTime * rotShieldSpeed);
+			transform.GetChild(0).rotation = Quaternion.Slerp(transform.GetChild(0).rotation, rotation, Time.deltaTime * rotSpeed);
 
 		}
 	}
@@ -163,7 +191,7 @@ public class InputController : MonoBehaviour {
 		if (targetDir.sqrMagnitude > 0.21f) {
 			// rotation of the ship
 			var rotation= Quaternion.LookRotation(targetDir, Vector3.up);
-			transform.GetChild(2).rotation = Quaternion.Slerp(transform.GetChild(2).rotation, rotation, Time.deltaTime * rotShieldSpeed);
+			transform.GetChild(2).rotation = Quaternion.Slerp(transform.GetChild(2).rotation, rotation, Time.deltaTime * rotRocketSpeed);
 		}
 	}
 
@@ -176,6 +204,16 @@ public class InputController : MonoBehaviour {
 		
 		Destroy(bullet , 3.0f);
 
+	}
+
+	void EnableBouncingShield() {
+
+		if(Input.GetAxis("BouncingShield") > 0.1f) {
+			canDeflect = true;
+		}
+		else {
+			canDeflect = false;
+		}
 	}
 
 }
